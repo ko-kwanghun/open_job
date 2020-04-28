cc64a7d43d975b60cf6c813b445fce94356bf267
ko-kwanghun <kanfunko@gmail.com>
2020-04-28 11:37:25 +0900

    444444

--- index1.php
+++ index1.php
@@ -1,19 +1,5 @@
 <?php
-/**
- * Requests collector.
- *
- *  This file collects requests if:
- *	- no mod_rewrite is avilable or .htaccess files are not supported
- *  - requires App.baseUrl to be uncommented in app/config/core.php
- *	- app/webroot is not set as a document root.
- *
- * PHP versions 4 and 5
- *
- * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
- * Copyright 2005-2012, Cake Software Foundation, Inc. (http://cakefoundation.org)
- *
- * Licensed under The MIT License
- * Redistributions of files must retain the above copyright notice.
+
  *
  * @copyright     Copyright 2005-2012, Cake Software Foundation, Inc. (http://cakefoundation.org)
  * @link          http://cakephp.org CakePHP(tm) Project
